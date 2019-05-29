7e30bd38f1f1f24750265178dce95fcb47b11de5
jennifer <jennifer@aion.network>
2019-05-29 18:07:00 -0400

    update dependency

--- ATSTokenContract/src/main/java/atsToken/ATSTokenContract.java
+++ ATSTokenContract/src/main/java/atsToken/ATSTokenContract.java
@@ -220,6 +220,8 @@ public class ATSTokenContract {
             }
             return false;
         }
+
+
 //
 //        private byte[] getData() {
 //            return this.currentTokenHolderInformation;

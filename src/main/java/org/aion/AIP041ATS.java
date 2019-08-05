dfe6ca9c458145a6f180e530b964fdd0684e1f88
jennifer <jennifer@aion.network>
2019-08-05 19:20:07 -0400

    update the wrapper

--- src/main/java/org/aion/AIP041ATS.java
+++ src/main/java/org/aion/AIP041ATS.java
@@ -8,24 +8,64 @@ import org.aion.avm.tooling.abi.Initializable;
 import java.math.BigInteger;
 import java.util.Arrays;
 
+/**
+ * Reference contract implementation for AIP-041 Aion fungible Token(https://github.com/aionnetwork/AIP/issues/41).
+ *
+ * This code is pending audit.
+ * Do not use or deploy this code before reviewing it personally first.
+ *
+ * @author:
+ * Jiaying Wang(https://github.com/jennijuju)
+ *
+ * @contributors:
+ * Yao Sun(https://github.com/qoire);
+ * Jeff Disher(https://github.com/jeff-aion);
+ * Aayush Rajasekaran(https://github.com/arajasek)
+ *
+ *            _____ ____  _   _   ______                 _ _     _        _______    _
+ *     /\   |_   _/ __ \| \ | |  |  ____|               (_) |   | |    |__   __|  | |
+ *    /  \    | || |  | |  \| |  | |__ _   _ _ __   __ _ _| |__ | | ___   | | ___ | | _____ _ __
+ *   / /\ \   | || |  | | . ` |  |  __| | | | '_ \ / _` | | '_ \| |/ _ \  | |/ _ \| |/ / _ \ '_ \
+ *  / ____ \ _| || |__| | |\  |  | |  | |_| | | | | (_| | | |_) | |  __/  | | (_) |   <  __/ | | |
+ * /_/    \_\_____\____/|_| \_|  |_|   \__,_|_| |_|\__, |_|_.__/|_|\___|  |_|\___/|_|\_\___|_| |_|
+ *                                                  __/ |
+ *                                                 |___/
+ *
+ */
 public class AIP041ATS {
 
 
     /**************************************Deployment Initialization***************************************/
+
+    /**
+     * tokenName: Name of the token.
+     */
     @Initializable
     private static String tokenName ;
 
+    /**
+     * tokenSymbol: Symbol of the token.
+     */
     @Initializable
     private static String tokenSymbol;
 
+    /**
+     * tokenGranularity: Granularity of the token.
+     * The granularity is the smallest number of tokens (in the basic unit, which is 10^-18) which MAY be minted, sent and burned in any transaction.
+     */
     @Initializable
     private static int tokenGranularity;
 
+    /**
+     * totalSupply: Total number of the minted token upon token creation.
+     */
     @Initializable
     private static BigInteger tokenTotalSupply;
 
+    /**
+     * Initialization upon deployment.
+     */
     static {
-
         Blockchain.require(tokenName.length() > 0);
         Blockchain.require(tokenSymbol.length() > 0);
         Blockchain.require(tokenGranularity >= 1);
@@ -36,7 +76,10 @@ public class AIP041ATS {
 
 
     /********************************************Initialization********************************************/
-
+    /**
+     * Total number of the minted token upon token creation is initialized under the token creator's account.
+     * "ATSTokenCreated" event is emitted upon deployment.
+     */
     private static void initialize() {
         Blockchain.putStorage(AIP041KeyValueStorage.getBalanceKey(Blockchain.getCaller()), tokenTotalSupply.toByteArray());
         AIP041Event.ATSTokenCreated(tokenTotalSupply, Blockchain.getCaller());
@@ -45,21 +88,37 @@ public class AIP041ATS {
 
     /**********************************************Token Info**********************************************/
 
+    /**
+     * Get the name of the token.
+     * @return The name of the token.
+     */
     @Callable
     public static String name() {
         return tokenName;
     }
 
+    /**
+     * Get the symbol of the token.
+     * @return The symbol of the token.
+     */
     @Callable
     public static String symbol() {
         return tokenSymbol;
     }
 
+    /**
+     * Get the granularity of the token.
+     * @return The granularity of the token.
+     */
     @Callable
     public static int granularity() {
         return tokenGranularity;
     }
 
+    /**
+     * Get the total supply of the token.
+     * @return The total supply of the token.
+     */
     @Callable
     public static BigInteger totalSupply() {
         return tokenTotalSupply;
@@ -67,6 +126,11 @@ public class AIP041ATS {
 
     /*********************************************Token Holder*********************************************/
 
+    /**
+     * Get the balance of an account.
+     * @param tokenHolder An account that has ownership over a token balance.
+     * @return The token balance of the account.
+     */
     @Callable
     public static BigInteger balanceOf(Address tokenHolder) {
         byte[] balance = Blockchain.getStorage(AIP041KeyValueStorage.getBalanceKey(tokenHolder));
@@ -75,6 +139,10 @@ public class AIP041ATS {
                 : BigInteger.ZERO;
     }
 
+    /**
+     * Set a third party operator address as an operator of Blockchain.getCaller() to send and burn tokens on its behalf.
+     * @param operator The address that Blockchain.getCaller() wants to authorize to send and burn tokens on its behalf in the future.
+     */
     @Callable
     public static void authorizeOperator(Address operator) {
         AIP041KeyValueStorage.getIsOperatorKey(operator,Blockchain.getCaller());

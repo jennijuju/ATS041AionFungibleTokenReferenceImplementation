a2833dd06d97eb82367d93686464c3b122e3687f
jennifer <jennifer@aion.network>
2019-08-05 20:05:04 -0400

    Add comments

--- src/main/java/org/aion/AIP041ATS.java
+++ src/main/java/org/aion/AIP041ATS.java
@@ -141,16 +141,23 @@ public class AIP041ATS {
 
     /**
      * Set a third party operator address as an operator of Blockchain.getCaller() to send and burn tokens on its behalf.
-     * @param operator The address that Blockchain.getCaller() wants to authorize to send and burn tokens on its behalf in the future.
+     * A token holder MAY authorize an already authorized operator.
+     * An AuthorizedOperator MUST be emitted each time.
+     * @param operator  Address to set as an operator for Blockchain.getCaller()
+     *
      */
     @Callable
     public static void authorizeOperator(Address operator) {
-
         Blockchain.putStorage(AIP041KeyValueStorage.getIsOperatorKey(operator,Blockchain.getCaller()), new byte[] {0x01});
         AIP041Event.AuthorizedOperator(operator, Blockchain.getCaller());
-
     }
 
+    /**
+     * Remove the right of the operator address to be an operator for Blockchain.getCaller() to send and burn tokens on its behalf.
+     * The token holder (Blockchain.getCaller()) is always an operator for itself. This right MUST NOT be revoked. This function MUST revert if it is called to revoke the token holder.
+     * A RevokeOperator MUST be emitted each time.
+     * @param operator  Address to rescind as an operator for Blockchain.getCaller().
+     */
     @Callable
     public static void revokeOperator(Address operator) {
         Blockchain.require(!operator.equals(Blockchain.getCaller()));
@@ -161,6 +168,13 @@ public class AIP041ATS {
 
     }
 
+    /**
+     * Indicate whether the operator address is an operator of the tokenHolder address.
+     * A token holder is always an operator of itself.
+     * @param operator Address which may be an operator of tokenHolder.
+     * @param tokenHolder Address of a token holder which may have the operator address as an operator.
+     * @return true if operator is an operator of tokenHolder and false otherwise.
+     */
     @Callable
     public static boolean isOperatorFor(Address operator, Address tokenHolder) {
         if (operator.equals(tokenHolder)) {
@@ -174,11 +188,29 @@ public class AIP041ATS {
     }
 
     /******************************************Token Movement*******************************************/
+
+    /**
+     * Send the amount of tokens from the address Blockchain.getCaller() to the address to.
+     * The operator and the token holder MUST both be the Blockchain.getCaller().
+     * @param to Token recipient.
+     * @param amount Number of tokens to send.
+     * @param userData Information attached to the send, and intended for the recipient (to).
+     */
     @Callable
     public static void send(Address to, BigInteger amount, byte[] userData) {
         doSend(Blockchain.getCaller(), Blockchain.getCaller(), to, amount, userData, new byte[0], true);
     }
 
+    /**
+     * Send the amount of tokens on behalf of the address from to the address to.
+     * The operator MUST be Blockchain.getCaller().
+     * The value of from MAY be 0x0, then the from (token holder) used for the send MUST be Blockchain.getCaller() (the operator).
+     * @param from Token holder.
+     * @param to Token recipient.
+     * @param amount  Number of tokens to send.
+     * @param userData Information attached to the send, and intended for the recipient (to).
+     * @param operatorData Information attached to the send by the operator.
+     */
     @Callable
     public static void operatorSend(Address from, Address to, BigInteger amount, byte[] userData, byte[] operatorData) {
         if (from.equals(new Address(new byte[32]))) {
@@ -189,12 +221,27 @@ public class AIP041ATS {
         }
     }
 
+    /**
+     * Burn the amount of tokens from the address Blockchain.getCaller().
+     * The operator and the token holder MUST both be the Blockchain.getCaller().
+     * @param amount Number of tokens to burn.
+     * @param holderData Information attached to the burn by the token holder.
+     */
     @Callable
     public static void burn(BigInteger amount, byte[] holderData) {
         doBurn(Blockchain.getCaller(),Blockchain.getCaller(), amount ,holderData, new byte[0]);
     }
 
-
+    /**
+     * Burn the amount of tokens on behalf of the address from.
+     * The operator MUST be Blockchain.getCaller().
+     * The value of from MAY be 0x0, then the from (token holder) used for the burn MUST be Blockchain.getCaller() (the operator).
+     * If the operator address is not an authorized operator of the from address, then the burn process MUST revert.
+     * @param tokenHolder Token holder whose tokens will be burned (or 0x0 to set from to Blockchain.getCaller()).
+     * @param amount Number of tokens to burn.
+     * @param holderData Information attached to the burn by the token holder.
+     * @param operatorData Information attached to the burn by the operator.
+     */
     @Callable
     public static void operatorBurn(Address tokenHolder, BigInteger amount, byte[] holderData, byte[] operatorData) {
         if (tokenHolder.equals(new Address(new byte[32]))) {
@@ -205,6 +252,7 @@ public class AIP041ATS {
         }
     }
 
+    
     private static void doSend(Address operator, Address from, Address to, BigInteger amount, byte[] userData, byte[] operatorData, boolean preventLocking) {
         Blockchain.require(amount.compareTo(BigInteger.ZERO) > -1); //Amount is not negative value
         Blockchain.require(amount.mod(BigInteger.valueOf(tokenGranularity)).equals(BigInteger.ZERO)); //Check granularity
